177f90bdb0f8e5503a0a3f0a7d712b50cd79348f
Stalise <atis.kozlov@mail.ru>
2023-01-06 13:03:59 +0300

    update db-service

--- src/services/db-service.ts
+++ src/services/db-service.ts
@@ -2,9 +2,9 @@ import pg from 'pg';
 
 const pool = new pg.Pool({
   user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
+  password: String(process.env.PG_PASSWORD),
   host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
+  port: Number(process.env.PG_PORT),
   database: process.env.PG_DATABASE,
 });
 
--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -2,7 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PG_USER: string;
-      PG_PASSWORD: string;
+      PG_PASSWORD: number;
       PG_HOST: string;
       PG_PORT: number;
       PG_DATABASE: string;
