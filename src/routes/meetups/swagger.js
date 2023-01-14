2dd4810284bb82dbf8a3c58f52b6da1a68de9403
Stalise <atis.kozlov@mail.ru>
2023-01-14 19:25:43 +0300

    add swagger schema for deleteMeetup method

--- src/routes/meetups/swagger.js
+++ src/routes/meetups/swagger.js
@@ -119,3 +119,32 @@
  *        500:
  *          $ref: '#/components/responses/UnexpectedError'
  */
+//=============================================================================//
+/**
+ *@swagger
+ *  /meetups/{id}:
+ *    delete:
+ *      summary: Delete meetup by id
+ *      parameters:
+ *        - in: path
+ *          description: The meetup id
+ *          required: true
+ *          name: id
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: The meetup was successfully deleted
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message: string
+ *                example:
+ *                  message: Meetup was successfully deleted!
+ *        404:
+ *          $ref: '#/components/responses/MeetupNotFound'
+ *        500:
+ *          $ref: '#/components/responses/UnexpectedError'
+ */
