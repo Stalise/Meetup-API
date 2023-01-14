5384e80dceceda790b61dd9f59800fcd4d60eddd
Stalise <atis.kozlov@mail.ru>
2023-01-14 19:19:19 +0300

    add swagger schema for updateMeetup method

--- src/routes/meetups/swagger.js
+++ src/routes/meetups/swagger.js
@@ -79,3 +79,43 @@
  *        500:
  *          $ref: '#/components/responses/UnexpectedError'
  */
+//=============================================================================//
+/**
+ *@swagger
+ *  /meetups:
+ *    patch:
+ *      summary: Update information about meetup
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              required:
+ *                - id
+ *              properties:
+ *                id:
+ *                  type: integer
+ *                theme:
+ *                  type: string
+ *                description:
+ *                  type: string
+ *                time:
+ *                  type: string
+ *                venue:
+ *                  type: string
+ *                tags:
+ *                  type: array
+ *                  items:
+ *                    type: string
+ *      responses:
+ *        200:
+ *          description: The meeting information has been successfully updated
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Meetup'
+ *        404:
+ *          $ref: '#/components/responses/MeetupNotFound'
+ *        500:
+ *          $ref: '#/components/responses/UnexpectedError'
+ */
