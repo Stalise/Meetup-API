c0bf409c8698be454302f5727296c1ab62abeddb
Stalise <atis.kozlov@mail.ru>
2023-01-14 18:17:24 +0300

    add schema for createMeetup method

--- src/routes/meetups/swagger.js
+++ src/routes/meetups/swagger.js
@@ -1,6 +1,45 @@
 /**
  *@swagger
  *  /meetups:
+ *    post:
+ *      summary: Create new meetup
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              required:
+ *                - theme
+ *                - description
+ *                - time
+ *                - venue
+ *              properties:
+ *                theme:
+ *                  type: string
+ *                description:
+ *                  type: string
+ *                time:
+ *                  type: string
+ *                venue:
+ *                  type: string
+ *                tags:
+ *                  type: array
+ *                  items:
+ *                    type: string
+ *      responses:
+ *        201:
+ *          description: Successful creation of a meetup
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Meetup'
+ *        500:
+ *          $ref: '#/components/responses/UnexpectedError'
+ */
+//=============================================================================//
+/**
+ *@swagger
+ *  /meetups:
  *    get:
  *      summary: Returns the list of all the meetups
  *      responses:
@@ -15,3 +54,4 @@
  *        500:
  *          $ref: '#/components/responses/UnexpectedError'
  */
+//=============================================================================//
