adebc3c0ab7a5514b17668d7876e7af2ee07c0f7
Stalise <atis.kozlov@mail.ru>
2023-01-14 18:52:31 +0300

    add schema for getMeetup method

--- src/data/swagger/responses.js
+++ src/data/swagger/responses.js
@@ -13,4 +13,15 @@
  *                type: string
  *            example:
  *              message: An unexpected error has occurred. Please tell us about this problem.
+ *    MeetupNotFound:
+ *      description: The meetup was not found by id
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              message:
+ *                type: string
+ *            example:
+ *              message: Meetup is not exist.
  */
--- src/routes/meetups/swagger.js
+++ src/routes/meetups/swagger.js
@@ -55,3 +55,27 @@
  *          $ref: '#/components/responses/UnexpectedError'
  */
 //=============================================================================//
+/**
+ *@swagger
+ *  /meetups/{id}:
+ *    get:
+ *      summary: Returns meetup by id
+ *      parameters:
+ *        - in: path
+ *          description: The meetup id
+ *          required: true
+ *          name: id
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: The meetup
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Meetup'
+ *        404:
+ *          $ref: '#/components/responses/MeetupNotFound'
+ *        500:
+ *          $ref: '#/components/responses/UnexpectedError'
+ */
