022d0d4a0896e6761b6a4bc179e7aae25320df5f
Stalise <atis.kozlov@mail.ru>
2023-01-15 15:25:41 +0300

    delete environment.t.ts file and some refactor init-db.ts

--- src/helpers/init-db.ts
+++ src/helpers/init-db.ts
@@ -2,7 +2,7 @@ import pg from 'pg';
 
 const pool = new pg.Pool({
   user: process.env.PG_USER,
-  password: String(process.env.PG_PASSWORD),
+  password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   port: Number(process.env.PG_PORT),
   database: process.env.PG_DATABASE,
